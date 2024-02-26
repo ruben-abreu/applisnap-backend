4cfc7fcb09d36244507122b6b5ee8d802f3ac31f
anapatriciagomes <anapatrigomes@gmail.com>
2024-02-26 16:40:58 +0000

    files cleanup

--- routes/roles.routes.js
+++ routes/roles.routes.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const router = require('express').Router();
 const User = require('../models/User.model');
-const Boards = require('../models/Boards.model');
-const Lists = require('../models/Lists.model');
-const Jobs = require('../models/Jobs.model');
 const Roles = require('../models/Roles.model');
 
 router.post('/roles', async (req, res, next) => {
